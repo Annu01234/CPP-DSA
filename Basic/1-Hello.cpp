43988071a6845be7b6980ddd4aecde98d738a58e
Anu <cannu2808@gmail.com>
2025-03-31 22:13:37 +0530

    Add files via upload

---
+++ Basic/1-Hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello Duniya..." << endl;
+    int a,b;
+    cout << "\nEnter the Value of a & b: ";
+    cin >> a >> b;
+    cout<< "Summation of A + B is " << a + b;
+    return 0;
+}
